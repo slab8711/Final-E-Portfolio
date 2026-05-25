7a8fda66ccea2bbee1d5cf51206f6574cd4b6dd4
slab8711 <scottlab@gmail.com>
2026-05-25 16:39:26 -0600

    Edit

--- index.js
+++ index.js
@@ -1,7 +1,33 @@
+let isModalOpen = false;
+let contrastToggle = false;
+const scaleFactor = 1 / 20;
+
+function moveBackground(event) {
+    const shapes = document.querySelectorAll(".shape");
+    const x = event.clientX * scaleFactor;
+    const y = event.clientY * scaleFactor;
+
+    for (let i=0; i < shapes.length; ++i) {
+        const isOdd = i % 2 !== 0;
+        const boolInt = isOdd ? -1 : 1;
+        shapes[i].style.transform = `translate(${x * boolInt}px, ${y * boolInt}px )`
+    }
+}
+
+function toggleContrast () {
+    contrastToggle = !contrastToggle;
+    if (contrastToggle) {
+        document.body.classList += " dark-theme"
+    }
+    else {
+        document.body.classList.remove("dark-theme")
+    }
+}
+
 function contact(event) {
     event.preventDefault();
-    const loading = document.querySelector('modal__overlay--loading');
-    const success = document.querySelector('modal__overlay--success');
+    const loading = document.querySelector('.modal__overlay--loading');
+    const success = document.querySelector('.modal__overlay--success');
     loading.classList += " modal__overlay--visible";
     emailjs
         .sendForm(
@@ -19,4 +45,13 @@ function contact(event) {
             "This email is temporarily unavailable. Please contact me directly on scottlab8711@gmail.com"
             );
         })
+}
+
+function toggleModal() {
+    if (isModalOpen) {
+        isModalOpen = false;
+        return document.body.classList.remove("modal--open");
+    }
+    isModalOpen = true;
+    document.body.classList += " modal--open";
 }
